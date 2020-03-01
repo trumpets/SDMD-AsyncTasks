35be79ec94c43d09f73fb1cb5fe44266b133b0cf
Ivo Neskovic <ivo.neskovic@gmail.com>
2020-03-01 16:21:47 +0100

    upgraded gradle and migrated to androidx.

--- app/src/main/java/gr/academic/city/sdmd/asynctasks/MainActivity.java
+++ app/src/main/java/gr/academic/city/sdmd/asynctasks/MainActivity.java
@@ -7,7 +7,6 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
@@ -17,6 +16,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
